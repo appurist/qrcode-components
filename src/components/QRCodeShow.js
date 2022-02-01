6ec78169de71942c9b9e6217776bf7158eb71c6e
Appurist / Paul <dev@appurist.com>
2022-02-01 13:49:21 -0400

    Just added the comment.

--- src/components/QRCodeShow.js
+++ src/components/QRCodeShow.js
@@ -1,5 +1,6 @@
 import QRCode from 'react-qr-code';
 
+// This version just provides a simple indirect reference to react-qr-code.
 function QRCodeShow(props) {
   return (
     <QRCode value={props.value} title={props.value} size={props.size}
